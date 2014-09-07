b05f5031cfdd92e2936d483b431414f6eefeae88
Christoph Jansen <jansen.christoph@yahoo.de>
2014-09-07 17:51:25 +0200

    VSA

--- StromaFramework/Program.cs
+++ StromaFramework/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VMscope.VirtualSlideAccess;
+using VMscope.InteropCore.ImageStreaming;
+using System.Drawing;
 
 namespace StromaFramework
 {
@@ -10,6 +13,10 @@ namespace StromaFramework
     {
         static void Main(string[] args)
         {
+            IStreamingImage image = Sdk.GetImage("D:\\mip_projekt\\S-114-HE\\S-114-HE.vsf");
+            Bitmap labels = image.GetLabelImage();
+            Bitmap tissue = image.GetTissueImage();
+            Console.WriteLine("done");
         }
     }
 }
